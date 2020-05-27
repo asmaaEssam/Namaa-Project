7027f74a5cfaf733fa40e5afe55a0cfb9a13241e
asmaaEssam <61593032+asmaaEssam@users.noreply.github.com>
2020-05-27 02:58:33 +0200

    creating the project model

---
+++ models/projectmodel.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const pointSchema = new mongoose.Schema({
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  });
+
+const Schema= new mongoose.Schema({
+    category:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    owner:{
+        type:String,
+        required:true
+    },
+    manager:{
+        type:String,
+        required:true
+    },
+    location: {
+        type: pointSchema,
+        required: true
+      },
+    start_date:{
+        type : Date,
+        required:true 
+    },
+    end_date:{
+        type : Date,
+        required:true
+    },
+    status:{
+        type:String,
+        required:true
+    },
+    employees:{
+        type: [String],
+        required: true
+    }
+    
+})
+
+module.exports= mongoose.model('Project', Schema)
