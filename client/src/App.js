79530acda0ddde19ac74414d9b5a9cc1af4ebd12
asmaaEssam <61593032+asmaaEssam@users.noreply.github.com>
2020-06-02 05:37:59 +0200

    creating project tab

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React, {useState} from 'react';
-
 import Bar from './components/Bar'
-import VerticalMenu from './components/VerticalMenu'
+import VerticalMenu from './components/VerticalMenu';
+import {Route , BrowserRouter} from "react-router-dom";
+import ProjectProfile from './components/ProjectProfile'
 
 function App() {
 
   const [isAddMenuState, setIsAddMenuState] = useState(false);
+  const [state, setState] = useState({
+    columns: [{ title: 'Category', field: 'category' },
+    { title: 'Name', field: 'name' },
+    { title: 'Owner', field: 'owner' },
+    { title: 'Manager', field: 'manager' },
+    { title: 'Status', field: 'status' },
+    { title: 'Start Date', field: 'start_date', type: 'date' },
+    { title: 'End Date', field: 'end_date', type:'date' },
+],
+    data: [
+    ],
+    project :[]
+  });
 
   function triggerAddMenuState() {
     setIsAddMenuState(!isAddMenuState)
@@ -13,8 +27,15 @@ function App() {
 
   return (
     <div className="App">
-      <Bar addMenu = {triggerAddMenuState}/>
-      {isAddMenuState && <VerticalMenu/>}
+      <BrowserRouter>
+      <Route exact path='/' render={props =>
+        <>
+        <Bar addMenu = {triggerAddMenuState}/>
+        {isAddMenuState && <VerticalMenu state={state} setState={setState} />}
+        </>
+        } />
+      <Route path='/project/:projectid' component= {ProjectProfile}/>
+        </BrowserRouter>
     </div>
   );
 }
